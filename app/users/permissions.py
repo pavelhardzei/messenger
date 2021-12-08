ed91c4cf82f3c44663c976df5e494df421b5c3c2
pavelhardzei <pavelgordei11@gmail.com>
2021-12-08 15:23:03 +0300

    updated

--- app/users/permissions.py
+++ app/users/permissions.py
@@ -3,7 +3,4 @@ from rest_framework import permissions
 
 class IsAdminOrOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
-            return True
-
-        return obj.id == request.user.id
+        return request.user.is_superuser or obj.id == request.user.id
