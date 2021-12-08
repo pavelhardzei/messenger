70ab3c7470f86f1494646ae2d84b6f5e64eed8d7
pavelhardzei <pavelgordei11@gmail.com>
2021-12-08 14:07:59 +0300

    updated

--- app/messenger_project/settings.py
+++ app/messenger_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'base',
     'users'
 ]
@@ -106,6 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 
 # Internationalization
