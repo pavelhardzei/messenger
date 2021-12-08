a7c4c0ae20318f3b144bf45812028d4d371c843d
pavelhardzei <pavelgordei11@gmail.com>
2021-12-08 14:09:23 +0300

    update urls

--- app/messenger_project/urls.py
+++ app/messenger_project/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('base/', include('base.urls')),
+    path('user/', include('users.urls')),
     path('admin/', admin.site.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ app/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from . import views
+
+
+urlpatterns = [
+    path('signup/', views.UserSignUp.as_view(), name='signup'),
+    path('signin/', views.UserSignIn.as_view(), name='signin'),
+    re_path(r'^(?P<pk>(\d+|me))/$', views.UserDetail.as_view(), name='user_detail'),
+    re_path(r'^change_pwd/(?P<pk>(\d+|me))/$', views.ChangePassword.as_view(), name='user_change_pwd')
+]
