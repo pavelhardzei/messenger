59da3ce8f11b6f458e8ee75fa5bca41de04c0f54
pavelhardzei <pavelgordei11@gmail.com>
2021-12-10 13:33:47 +0300

    update urls

--- app/messenger_project/urls.py
+++ app/messenger_project/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('base/', include('base.urls')),
     path('user/', include('users.urls')),
+    path('room/', include('rooms.urls')),
     path('admin/', admin.site.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
