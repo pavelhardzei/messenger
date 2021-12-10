16ea7e55e5b6e2fae91494d8a53e4036553d4a4c
pavelhardzei <pavelgordei11@gmail.com>
2021-12-10 11:57:12 +0300

    added room model

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import UserProfile
 
-# Create your models here.
+
+class Room(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class RoomUser(models.Model):
+    class Role(models.TextChoices):
+        member = ('member', 'MEMBER')
+        moderator = ('moderator', 'MODERATOR')
+        owner = ('owner', 'OWNER')
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    role = models.CharField(max_length=15, choices=Role.choices)
+
+    def __str__(self):
+        return f'Room: {self.room}, user: {self.user}'
