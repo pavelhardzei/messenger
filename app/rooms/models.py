d0d18072c19e287d3aba2ce090bd36d3638c0e38
Pavel Hardzei <79306458+pavelhardzei@users.noreply.github.com>
2021-12-14 09:24:56 +0300

    Room model (#4)
    * fixed
    
    * updated
    
    * added serializers
    
    * added permissions
    
    * added endpoints
    
    * update urls
    
    * updated
    
    * replaced with absolute imports
    
    * updated
    
    * updated
    
    * added isort, flake8 + autopep8
    
    * added args
    
    * modified
    
    * added pytest
    
    * added pytest configuration
    
    * added tests
    
    * fixed
    
    * added rooms app
    
    * added room model
    
    * update
    
    * update
    
    * update
    
    * update

---
+++ app/rooms/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from users.models import UserProfile
+
+
+class Room(models.Model):
+    class RoomType(models.TextChoices):
+        open = ('open', 'OPEN')
+        closed = ('closed', 'CLOSED')
+        private = ('private', 'PRIVATE')
+
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    room_type = models.CharField(max_length=15, choices=RoomType.choices, default=RoomType.open)
+
+    def __str__(self):
+        return self.title
+
+
+class RoomUser(models.Model):
+    class Role(models.TextChoices):
+        member = ('member', 'MEMBER')
+        moderator = ('moderator', 'MODERATOR')
+        owner = ('owner', 'OWNER')
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    role = models.CharField(max_length=15, choices=Role.choices)
+
+    def __str__(self):
+        return f'Room: {self.room}, user: {self.user}'
