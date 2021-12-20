97eb0b0b85ca9a9019913fb713cfffc1e277a197
pavelhardzei <pavelgordei11@gmail.com>
2021-12-20 11:23:28 +0300

    updated tests

--- app/rooms/tests.py
+++ app/rooms/tests.py
@@ -58,6 +58,29 @@ def test_set_role(api_client, user1, user2, room_open):
     assert response.status_code == status.HTTP_200_OK
 
 
+def test_invitation(api_client, user1, user3, room_open, room_closed):
+    api_client.force_authenticate(user3)
+    response = api_client.post('/api/room/invitation/', {'room': room_open.id})
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    api_client.force_authenticate(user1)
+    response = api_client.post('/api/room/invitation/', {'room': room_closed.id})
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    response = api_client.post('/api/room/invitation/', {'room': room_open.id})
+    assert response.status_code == status.HTTP_201_CREATED
+    assert set(response.json()) == {'id', 'room', 'created', 'expiration'}
+
+    uuid4 = response.json()['id']
+    response = api_client.post(f'/api/room/invitation/{uuid4}/')
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    api_client.force_authenticate(user3)
+    response = api_client.post(f'/api/room/invitation/{uuid4}/')
+    assert response.status_code == status.HTTP_200_OK
+    assert len(room_open.users.all()) == 3
+
+
 def test_permissions(rf, user1, user2, room_open):
     permission = IsMember()
 
