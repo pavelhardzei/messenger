cecfde2c6a8eede5cad0a1db71d7d9173303daad
pavelhardzei <pavelgordei11@gmail.com>
2021-12-21 13:20:27 +0300

    update field

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -2,7 +2,6 @@ import datetime
 import uuid
 
 from django.db import models
-from django.utils import timezone
 from users.models import UserProfile
 
 
@@ -40,5 +39,5 @@ class RoomUser(models.Model):
 class Invitation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='invitations')
-    created = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(default=datetime.datetime.utcnow)
     expiration = models.DurationField(default=datetime.timedelta(days=1))
