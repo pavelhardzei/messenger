44f8c76c551b3d0376b75f9abc78a32429b20e23
pavelhardzei <pavelgordei11@gmail.com>
2021-12-22 11:00:04 +0300

    removed comments

--- app/rooms/conftest.py
+++ app/rooms/conftest.py
@@ -35,38 +35,3 @@ def check_object_permissions_mock():
 def api_client(django_db_blocker):
     with django_db_blocker.unblock():
         yield APIClient()
-
-
-# @pytest.fixture(scope='module')
-# def user1(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         yield UserProfile.objects.create_user('test3@test.org', 'test3', 'TEST3', '2000-01-01', 'testing321')
-#
-#
-# @pytest.fixture(scope='module')
-# def user2(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         yield UserProfile.objects.create_user('test4@test.org', 'test4', 'TEST4', '2000-01-01', 'testing321')
-#
-#
-# @pytest.fixture(scope='module')
-# def user3(django_db_blocker):
-#     with django_db_blocker.unblock():
-#         yield UserProfile.objects.create_user('test5@test.org', 'test5', 'TEST5', '2000-01-01', 'testing321')
-#
-#
-# @pytest.fixture(scope='module')
-# def room_open(django_db_blocker, user1, user2):
-#     with django_db_blocker.unblock():
-#         room = Room.objects.create(title='room1', description='some_info', room_type=Room.RoomType.open)
-#         RoomUser.objects.create(room=room, user=user1, role=RoomUser.Role.owner)
-#         RoomUser.objects.create(room=room, user=user2, role=RoomUser.Role.member)
-#         yield room
-#
-#
-# @pytest.fixture(scope='module')
-# def room_closed(django_db_blocker, user1):
-#     with django_db_blocker.unblock():
-#         room = Room.objects.create(title='room2', description='some_info', room_type=Room.RoomType.closed)
-#         RoomUser.objects.create(room=room, user=user1, role=RoomUser.Role.owner)
-#         yield room
