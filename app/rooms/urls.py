5e313f19dadbe98b9df01520a69b06de050267e9
pavelhardzei <pavelgordei11@gmail.com>
2021-12-22 11:10:43 +0300

    update path

--- app/rooms/urls.py
+++ app/rooms/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('remove/', views.RemoveUser.as_view(), name='remove_user'),
     path('setrole/', views.SetRole.as_view(), name='set_role'),
     path('invitation/', views.MakeInvitation.as_view(), name='make_invitation'),
-    re_path(r'^invitation/(?P<uuid4>([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-4[0-9A-Fa-f]{3}-[89ABab][0-9A-Fa-f]{3}'
-            r'-[0-9A-Fa-f]{12}))/$', views.AcceptInvitation.as_view(), name='accept_invitation')
+    path('invitation/<uuid:uuid4>/', views.AcceptInvitation.as_view(), name='accept_invitation')
 ]
