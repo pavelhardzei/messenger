81955c3bedc598ca2d490cd2564f9024a727098f
pavelhardzei <pavelgordei11@gmail.com>
2021-12-22 14:25:01 +0300

    added crud endpoints

--- app/room_messages/views.py
+++ app/room_messages/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from room_messages.models import Message
+from room_messages.permissions import IsSender
+from room_messages.serializers import MessageSerializer
+from rooms.models import RoomUser
 
-# Create your views here.
+
+class MessageCreate(generics.CreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request, *args, **kwargs):
+        if not RoomUser.objects.filter(room=request.data['room'], user=request.user.id).exists():
+            return Response({'error_message': 'You are not in this room'}, status=status.HTTP_403_FORBIDDEN)
+
+        return super().post(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+
+class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = (IsSender, )
