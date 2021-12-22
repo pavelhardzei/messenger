9998ae8cfa14e674e1b476b8eb6ad02cf7d2a727
pavelhardzei <pavelgordei11@gmail.com>
2021-12-22 13:57:39 +0300

    update datetime field

--- app/room_messages/models.py
+++ app/room_messages/models.py
@@ -1,5 +1,6 @@
+import datetime
+
 from django.db import models
-from django.utils import timezone
 from rooms.models import Room
 from users.models import UserProfile
 
@@ -8,5 +9,5 @@ class Message(models.Model):
     text = models.CharField(max_length=300)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=datetime.datetime.utcnow)
+    updated_at = models.DateTimeField(default=datetime.datetime.utcnow)
