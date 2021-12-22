6d76309f0346ef544da0de9c48742e9b42a49a20
pavelhardzei <pavelgordei11@gmail.com>
2021-12-22 14:23:37 +0300

    added serializer

---
+++ app/room_messages/serializers.py
@@ -0,0 +1,29 @@
+import datetime
+
+from rest_framework import serializers
+from room_messages.models import Message
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.ReadOnlyField(source='sender.id')
+
+    class Meta:
+        model = Message
+        fields = ('id', 'text', 'room', 'sender', 'created_at', 'updated_at')
+
+        extra_kwargs = {'created_at': {'read_only': True},
+                        'updated_at': {'read_only': True}}
+
+    def get_fields(self, *args, **kwargs):
+        fields = super().get_fields()
+        request = self.context.get('request', None)
+        if request and (request.method == 'PUT' or request.method == 'PATCH'):
+            fields['room'].read_only = True
+        return fields
+
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data)
+        instance.updated_at = datetime.datetime.utcnow()
+        instance.save()
+
+        return instance
