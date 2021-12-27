b0e5155d4361ae220404102ad9d88b682a6ad5fb
pavelhardzei <pavelgordei11@gmail.com>
2021-12-27 17:51:27 +0300

    updated

--- app/room_messages/conftest.py
+++ app/room_messages/conftest.py
@@ -1,8 +1,5 @@
-from unittest.mock import patch
-
-import pytest
+from base.conftest import api_client, check_object_permissions_mock, enable_db_access_for_all_tests
 from pytest_factoryboy import LazyFixture, register
-from rest_framework.test import APIClient
 from room_messages.factories import MessageFactory
 from rooms.factories import RoomFactory, RoomUserFactory
 from users.factories import UserFactory
@@ -17,20 +14,3 @@ register(RoomUserFactory, 'room_user1', room=LazyFixture('room1'), user=LazyFixt
 register(RoomUserFactory, 'room_user2', room=LazyFixture('room1'), user=LazyFixture('user2'))
 
 register(MessageFactory, 'message_user1', room=LazyFixture('room1'), sender=LazyFixture('user1'))
-
-
-@pytest.fixture(autouse=True)
-def enable_db_access_for_all_tests(db):
-    pass
-
-
-@pytest.fixture(autouse=True)
-def check_object_permissions_mock():
-    with patch('rest_framework.views.APIView.check_object_permissions'):
-        yield True
-
-
-@pytest.fixture(scope='module')
-def api_client(django_db_blocker):
-    with django_db_blocker.unblock():
-        yield APIClient()
