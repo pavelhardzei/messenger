2cdce950c7bd2190b560241a85088948659d635b
pavelhardzei <pavelgordei11@gmail.com>
2022-01-05 17:57:01 +0300

    fixed

--- app/room_messages/tests.py
+++ app/room_messages/tests.py
@@ -11,7 +11,7 @@ def test_message_create(api_client, user1, user3, room1, room_user1):
     response = api_client.post('/api/message/', {'text': 'some text', 'room': room1.id})
     assert response.status_code == status.HTTP_201_CREATED
 
-    msg = Message.objects.all().first()
+    msg = Message.objects.get(sender=user1)
     assert response.json() == {'id': ANY, 'text': 'some text', 'room': room1.id,
                                'sender': user1.id,
                                'created_at': f'{msg.created_at.strftime(DATETIME_FMT)}',
@@ -27,7 +27,7 @@ def test_message_update(api_client, user1, room1, message_user1):
     response = api_client.put(f'/api/message/{message_user1.id}/', {'text': 'updated text'})
     assert response.status_code == status.HTTP_200_OK
 
-    msg = Message.objects.all().first()
+    msg = Message.objects.get(sender=user1)
     assert response.json() == {'id': ANY, 'text': 'updated text', 'room': room1.id,
                                'sender': user1.id,
                                'created_at': f'{msg.created_at.strftime(DATETIME_FMT)}',
