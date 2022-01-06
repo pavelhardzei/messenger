99d59048eedd75be1e51521bb3e74579996a3b41
pavelhardzei <pavelgordei11@gmail.com>
2022-01-06 18:34:20 +0300

    updated urls

--- app/users/urls.py
+++ app/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from users import views
 
 urlpatterns = [
+    path('', views.UserList.as_view(), name='user_list'),
     path('signup/', views.UserSignUp.as_view(), name='signup'),
     path('signin/', views.UserSignIn.as_view(), name='signin'),
     re_path(r'^(?P<pk>(\d+|me))/$', views.UserDetail.as_view(), name='user_detail'),
