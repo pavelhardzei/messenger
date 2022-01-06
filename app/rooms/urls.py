79765f6eb8091718be9c66d34b6630aa7e36a460
pavelhardzei <pavelgordei11@gmail.com>
2022-01-06 19:26:32 +0300

    update urls

--- app/rooms/urls.py
+++ app/rooms/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:pk>/leave/', views.LeaveRoom.as_view(), name='leave_room'),
     path('<int:room_pk>/user/<int:user_pk>/', views.RemoveUser.as_view(), name='remove_user'),
     path('<int:room_pk>/user/<int:user_pk>/role/', views.SetRole.as_view(), name='set_role'),
+    path('find/', views.RoomFinding.as_view(), name='find_room'),
     path('invitation/', views.MakeInvitation.as_view(), name='make_invitation'),
     path('invitation/<uuid:uuid4>/', views.AcceptInvitation.as_view(), name='accept_invitation')
 ]
