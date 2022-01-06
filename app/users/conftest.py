045e9012f4ec9fe79a1a35228620b46df84d6ad7
pavelhardzei <pavelgordei11@gmail.com>
2022-01-06 18:34:31 +0300

    updated tests

--- app/users/conftest.py
+++ app/users/conftest.py
@@ -1,6 +1,9 @@
 from base.conftest import api_client, check_object_permissions_mock, enable_db_access_for_all_tests
 from pytest_factoryboy import register
+from rooms.factories import RoomUserFactory
 from users.factories import UserFactory
 
 register(UserFactory, 'user1')
 register(UserFactory, 'user2')
+
+register(RoomUserFactory)
--- app/users/tests.py
+++ app/users/tests.py
@@ -1,3 +1,5 @@
+from django.db import connection
+from django.test.utils import CaptureQueriesContext
 from rest_framework import status
 from users.permissions import IsAdminOrOwner
 
@@ -13,13 +15,22 @@ def test_signin(api_client, user1):
     assert response.status_code == status.HTTP_200_OK
 
 
+def test_list(api_client, user1):
+    api_client.force_authenticate(user1)
+    response = api_client.get(f'/api/user/?email={user1.email}')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [{'id': user1.id, 'email': user1.email, 'user_name': user1.user_name,
+                                'full_name': user1.full_name, 'date_of_birth': f'{user1.date_of_birth}',
+                                'rooms': [], 'date_joined': f'{user1.date_joined}'}]
+
+
 def test_detail(api_client, user1, user2):
     api_client.force_authenticate(user1)
     response = api_client.get('/api/user/me/')
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'id': user1.id, 'email': user1.email, 'user_name': user1.user_name,
                                'full_name': user1.full_name, 'date_of_birth': f'{user1.date_of_birth}',
-                               'date_joined': f'{user1.date_joined}'}
+                               'rooms': [], 'date_joined': f'{user1.date_joined}'}
 
     response = api_client.patch('/api/user/me/')
     assert response.status_code == status.HTTP_200_OK
@@ -42,3 +53,35 @@ def test_permissions(rf, user1, user2):
     rf.delete(f'/api/user/{user2.pk}/')
     rf.user = user1
     assert not permission.has_object_permission(rf, None, user2)
+
+
+def test_get_user_detail_db_calls(api_client, user1, room_user_factory):
+    api_client.force_authenticate(user1)
+
+    room_user_factory.create(user=user1)
+    with CaptureQueriesContext(connection) as query_context:
+        response = api_client.get('/api/user/me/')
+    assert response.status_code == status.HTTP_200_OK
+    assert len(query_context) == 3
+
+    room_user_factory.create_batch(10, user=user1)
+    with CaptureQueriesContext(connection) as query_context:
+        response = api_client.get('/api/user/me/')
+    assert response.status_code == status.HTTP_200_OK
+    assert len(query_context) == 3
+
+
+def test_get_user_list_db_calls(api_client, user1, room_user_factory):
+    api_client.force_authenticate(user1)
+
+    room_user_factory.create_batch(10)
+    with CaptureQueriesContext(connection) as query_context:
+        response = api_client.get('/api/user/')
+    assert response.status_code == status.HTTP_200_OK
+    assert len(query_context) == 3
+
+    room_user_factory.create_batch(20)
+    with CaptureQueriesContext(connection) as query_context:
+        response = api_client.get('/api/user/')
+    assert response.status_code == status.HTTP_200_OK
+    assert len(query_context) == 3
