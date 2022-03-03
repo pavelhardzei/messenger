4a2b5c8d8368eb928d45578e0480c24eb6639655
pavelhardzei <pavelgordei11@gmail.com>
2022-03-03 16:53:59 +0300

    added email config

--- app/messenger_project/settings.py
+++ app/messenger_project/settings.py
@@ -200,3 +200,10 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 SOCIALACCOUNT_ADAPTER = 'users.adapters.SocialAdapter'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
