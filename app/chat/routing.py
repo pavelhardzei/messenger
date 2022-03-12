07a00af4a1a20ea63beae701536156671e4bb6ee
pavelhardzei <pavelgordei11@gmail.com>
2022-03-12 22:31:05 +0300

    added UserConsumer

--- app/chat/routing.py
+++ app/chat/routing.py
@@ -1,6 +1,7 @@
-from chat import consumers
+from chat.consumers import ChatConsumer, UserConsumer
 from django.urls import re_path
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<pk>\d+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<pk>\d+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/user/$', UserConsumer.as_asgi())
 ]
